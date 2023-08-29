050777aa2368a2f566905480ac57a479bba95905
EvelinRodriguezSmeding <evelinrodriguezsmeding@gotmail.com>
2023-08-28 22:12:37 -0300

    Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,11 +4,17 @@ import Tag from "../../../../../../public/icons/tag.svg";
 function Modal() {
   return (
     <>
-      <div className="miModal">
-        <button>
-          <img src={Tag} alt="" />
-        </button>
-        
+      <div className="myModal">
+        <div className="modal-container">
+          <button>
+            <img src={Tag} alt="" />
+          </button>
+          <p>Sort by:</p>
+          <input type="radio" />
+          <label htmlFor="opcion1">Number</label>
+          <input type="radio" />
+          <label htmlFor="opcion2">Name</label>
+        </div>
       </div>
     </>
   );
