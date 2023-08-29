770da776c8c5082ace49cb3b31f1ff0e4c252b17
EvelinRodriguezSmeding <evelinrodriguezsmeding@gotmail.com>
2023-08-28 21:18:42 -0300

    Add components and structure

---
+++ src/components/Modal.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Tag from "../../../../../../public/icons/tag.svg";
+
+function Modal() {
+  return (
+    <>
+      <div className="miModal">
+        <button>
+          <img src={Tag} alt="" />
+        </button>
+        
+      </div>
+    </>
+  );
+}
+
+export default Modal;
