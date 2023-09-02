5b4736060c6061ef81402cd3a5a41f7232762ddb
EvelinRodriguezSmeding <evelinrodriguezsmeding@gotmail.com>
2023-09-01 22:32:17 -0300

    Fix css in Home and add Link in Pokecard

--- src/components/Pokecard.jsx
+++ src/components/Pokecard.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import "./Pokecard.css";
-
+import { Link } from "react-router-dom";
 
 function Pokecard(props) {
   return (
     <>
-      <div className="pokecard-container">
-        <p>#{props.id}</p>
-        <img className="" src={props.image} alt="" />
-        <p><b>{props.name}</b></p>
-      </div>
+      <Link to={`/home/pokemon-details/${props.id}`} style={{textDecoration: 'none', color: '#212121'}}>
+        <div className="pokecard-container">
+          <p>#{props.id}</p>
+          <img className="" src={props.image} alt="" />
+          <p>
+            <b>{props.name}</b>
+          </p>
+        </div>
+      </Link>
     </>
   );
 }
